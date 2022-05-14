9b753bf100225af5b087700ed25b7e00f98f64a7
zTuanNg <tuann.nm241@gmail.com>
2022-05-14 15:38:01 +0700

    fix

--- FPT_shop/src/main/java/App.java
+++ FPT_shop/src/main/java/App.java
@@ -1,26 +1,6 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-
 public class App {
     public static void main(String[] args) {
-
         ProductController controller = new ProductController();
         controller.run();
-
-    }
-
-    public static class FilmService {
-
-        // show films
-        public void show(List<Film>lst){
-            lst.forEach(f-> System.out.println(f));
-        }
-
-        // sort by release day
-        public List<Film> sortByReleaseDay(List<Film> lst){
-            return lst.stream().sorted(Comparator.comparing(f->f.getRelease_day())).collect(Collectors.toList());
-        }
     }
 }
